99b1e1dcbdbf2c448fbbb2c9a9bc5d6b73e1e5be
Munkh113 <mathmonhoo@yahoo.com>
2020-07-08 23:15:05 +0800

    site updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,10 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Сайн уу, миний нэрийг Мөнх-Очир гэдэг</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
+                <p>Тавтай морилно уу<br />
                 and released under the Creative Commons.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
