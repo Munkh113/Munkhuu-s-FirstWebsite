16ece0855df596eef70f6110c2e9453ba405cbc2
Munkh113 <mathmonhoo@yahoo.com>
2020-07-08 23:29:17 +0800

    site updated again

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,11 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Сайн уу, миний нэрийг Мөнх-Очир гэдэг</h1>
+                <h1>Монголын хөгжүүлэх хүн бүрт зориулав</h1>
             </header>
             <div className="content">
                 <p>Тавтай морилно уу<br />
-                and released under the Creative Commons.</p>
+                Тухтай үзээрэй</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
                 </ul>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,8 +6,8 @@ const Menu = (props) => (
     <nav id="menu">
         <div className="inner">
             <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/">Эхлэл</Link></li>
+                <li><a href="../game/game.html">Тоглоом тоглох</a></li>
                 <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
             </ul>
