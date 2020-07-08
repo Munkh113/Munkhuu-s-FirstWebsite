5f20423772ec97b98b376e6baa59459661d84719
Munkh113 <mathmonhoo@yahoo.com>
2020-07-09 00:04:29 +0800

    neew

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,20 +6,20 @@ const Contact = (props) => (
             <section>
                 <form method="post" action="#">
                     <div className="field half first">
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="name">Нэр</label>
                         <input type="text" name="name" id="name" />
                     </div>
                     <div className="field half">
-                        <label htmlFor="email">Email</label>
+                        <label htmlFor="email">Имэйл</label>
                         <input type="text" name="email" id="email" />
                     </div>
                     <div className="field">
-                        <label htmlFor="message">Message</label>
+                        <label htmlFor="message">Бичих зүйлээ бичнэ үү</label>
                         <textarea name="message" id="message" rows="6"></textarea>
                     </div>
                     <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
+                        <li><input type="submit" value="Явуулах" className="special" /></li>
+                        <li><input type="reset" value="Арилгах" /></li>
                     </ul>
                 </form>
             </section>
@@ -27,24 +27,24 @@ const Contact = (props) => (
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
-                        <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
+                        <h3>Миний имэйл</h3>
+                        <a href="#">mathmonhoo@yahoo.com</a>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
-                        <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
+                        <h3>Утас</h3>
+                        <span>(+976) 99607331</span>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
+                        <h3>Хаяг</h3>
+                        <span>Монгол улс, Улаанбаатар<br />
+                        Хан-Уул дүүрэг, 16-р хороо<br />
+                        Буянт-Ухаа 1</span>
                     </div>
                 </section>
             </section>
