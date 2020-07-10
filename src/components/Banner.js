6d637191b631adb5d53ca743390dc218943d5fce
Munkh113 <mathmonhoo@yahoo.com>
2020-07-10 20:14:11 +0800

    pic01

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,13 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Монголын хөгжүүлэх хүн бүрт зориулав</h1>
+                <h1>Монголыг хөгжүүлэх хүн бүрт зориулав</h1>
             </header>
             <div className="content">
                 <p>Тавтай морилно уу<br />
                 Тухтай үзээрэй</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">Миний амьдрал</a></li>
                 </ul>
             </div>
         </div>
