8c32b76967fd7929c67ac929646a4c1b3dc3ae57
Munkh113 <mathmonhoo@yahoo.com>
2020-07-11 16:06:07 +0800

    pic4

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,8 +52,8 @@ class HomeIndex extends React.Component {
                         </article>
                         <article style={{backgroundImage: `url(${pic04})`}}>
                             <header className="major">
-                                <h3>Ipsum</h3>
-                                <p>Nisl sed aliquam</p>
+                                <h3>Гэр бүл</h3>
+                                <p>Аав ээж ах 4</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
