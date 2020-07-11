9134279c84e465915f87708b8308dd23923b6f6d
Munkh113 <mathmonhoo@yahoo.com>
2020-07-11 16:00:20 +0800

    banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import { Link } from 'gatsby'
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></Link>
+        <Link to="/" className="logo"><strong>ReMunkh</strong> <span>by Munkh-Ochir</span></Link>
         <nav>
-            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
+            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Цэс</a>
         </nav>
     </header>
 )
