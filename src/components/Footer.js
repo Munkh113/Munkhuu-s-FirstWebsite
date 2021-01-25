6088e8d43c3f5b0c95e65eda4450b0792e67eda7
Munkh-Ochir G <64304977+Munkh113@users.noreply.github.com>
2021-01-26 00:08:00 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="https://www.facebook.com/monh.ochir.1113" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="https://www.instagram.com/remunkh/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href="https://www.facebook.com/munkh.113/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/munkh.113/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                 <li><a href="https://github.com/Munkh113" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
             </ul>
             <ul className="copyright">
